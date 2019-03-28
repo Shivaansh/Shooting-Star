b5ec4d06e884f56265fa9f0ef925ab9746a20d33
Asal <kamala6@mcmaster.ca>
2019-03-28 19:38:13 -0400

    [AK] Initial Main Menu UI  with Buttons

---
+++ Assets/MainMenuButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("we want to quit");
+        Application.Quit();
+    }
+}

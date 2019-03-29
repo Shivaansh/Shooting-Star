8c2bc24be4a9f42bde8683f282bb5b5eb1a40a35
Asal <kamala6@mcmaster.ca>
2019-03-29 18:21:13 -0400

    [AK] New instructions panel

---
+++ Assets/_Complete-Game/Scripts/Managers/SessionController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class SessionController: MonoBehaviour
+
+    {
+    Canvas canvas;
+
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            canvas.enabled = !canvas.enabled;
+        }
+    }
+
+  
+    public void Quit()
+    {
+        //EditorApplication.isPlaying = false;
+
+		Application.Quit();
+
+    }
+    }
+}

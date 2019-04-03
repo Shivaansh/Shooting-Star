b772f2098fa1012fdb32ea0a59d64071ad96940a
Asalat <asalat.kamal@perkinelmer.com>
2019-04-02 20:32:17 -0400

    [AK] room choice spawning

---
+++ Assets/RoomChoicesController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RoomChoicesController : MonoBehaviour
+{
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
+
+    Canvas canvas;
+
+    void Start()
+    {
+        canvas = GameObject.Find("RoomChoicesCanvas").GetComponent<Canvas>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+        canvas.enabled = !canvas.enabled;
+        Pause();
+    }
+    public void Pause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        Lowpass();
+
+    }
+
+    void Lowpass()
+    {
+        if (Time.timeScale == 0)
+        {
+            paused.TransitionTo(.01f);
+        }
+
+        else
+
+        {
+            unpaused.TransitionTo(.01f);
+        }
+    }
+
+
+    //void OnGUI()
+    //{
+    //    DrawInfo();
+    //}
+}
--- Assets/_Complete-Game/Scripts/Managers/BossManager.cs
+++ Assets/_Complete-Game/Scripts/Managers/BossManager.cs
@@ -9,13 +9,15 @@ namespace CompleteProject
         public float spawnTime = 3f;            // How long between each spawn.
         public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
         ScoreManager scoreObject;
-        [SerializeField] int bossTriggerScore = 500;
+        [SerializeField] int bossTriggerScore = 20;
         int numberOfBosses = 1;
 
         [SerializeField] GameObject room1Fab; //the prefab for the 1st room choice object
         [SerializeField] GameObject room2Fab; //the prefab for the 2nd room choice object
+        [SerializeField] GameObject roomCentreFab; //the prefab for the room centre object
         [SerializeField] Transform room1Point;  //the transform for the 1st room choice object
         [SerializeField] Transform room2Point; //the transform for the 1st room choice object
+        [SerializeField] Transform roomCentrePoint; //the transform for the room centre object
 
 
         void Start()
@@ -51,6 +53,7 @@ namespace CompleteProject
             //Create instances of roomChoice prefabs and spawn at the chosen location
             Instantiate(room1Fab, room1Point.position, room1Point.rotation);
             Instantiate(room2Fab, room2Point.position, room2Point.rotation);
+            Instantiate(roomCentreFab, roomCentrePoint.position, roomCentrePoint.rotation);
         }
     }
 }

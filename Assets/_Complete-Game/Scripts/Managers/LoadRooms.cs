81d180154646e09c12a8dbc9c2a15cf3b07a03dd
Asalat <asalat.kamal@perkinelmer.com>
2019-04-02 20:40:38 -0400

    [EH] Room choices directing to scenes

---
+++ Assets/_Complete-Game/Scripts/Managers/LoadRooms.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadRooms : MonoBehaviour
+{
+    public void changeToScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
+
